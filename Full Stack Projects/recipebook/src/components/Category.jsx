7f7c0dc93f41235ae238962f429a750b13f19477
baron-gr <barongracias07@yahoo.co.uk>
2023-03-07 23:33:34 +0000

    07/03 updates

--- Full Stack Projects/recipebook/src/components/Category.jsx
+++ Full Stack Projects/recipebook/src/components/Category.jsx
@@ -7,22 +7,22 @@ import { NavLink } from 'react-router-dom';
 function Category() {
   return (
     <List>
-        <div>
+        <SLink to={"/cuisine/Italian"}>
             <FaPizzaSlice />
             <h4>Italian</h4>
-        </div>
-        <div>
+        </SLink>
+        <SLink to={"/cuisine/American"}>
             <FaHamburger />
             <h4>American</h4>
-        </div>
-        <div>
+        </SLink>
+        <SLink to={"/cuisine/Thai"}>
             <GiNoodles />
             <h4>Thai</h4>
-        </div>
-        <div>
+        </SLink>
+        <SLink to={"/cuisine/Japanese"}>
             <GiChopsticks />
             <h4>Japanese</h4>
-        </div>
+        </SLink>
     </List>
   )
 }
@@ -32,5 +32,37 @@ const List = styled.div`
     justify-content: center;
     margin: 2rem 0rem
 `;
+const SLink = styled(NavLink)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    margin-right: 2rem;
+    aspect-ratio: none;
+    text-decoration: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    width: 6rem;
+    height: 6rem;
+    cursor: pointer;
+    transform: scale(0.8);
+    h4 {
+        font-size: 0.9rem;
+        color: white;
+    }
+    svg {
+        font-size: 2rem;
+        color: white;
+    }
+    &.active {
+        background: linear-gradient(to right, #f27121, #e94057);
 
+        svg {
+            color: white;
+        }
+        h4 {
+            color: white;
+        }
+    }
+`;
 export default Category
