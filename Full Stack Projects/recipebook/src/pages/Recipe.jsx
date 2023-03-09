f7f5da53ca0a455803c7a985ff25a590fa29333d
baron-gr <barongracias07@yahoo.co.uk>
2023-03-09 16:16:53 +0000

    09/03 updates

--- Full Stack Projects/recipebook/src/pages/Recipe.jsx
+++ Full Stack Projects/recipebook/src/pages/Recipe.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 
 function Recipe() {
+
     let params = useParams();
     const [details, setDetails] = useState({});
+    const [activeTab, setActiveTab] = useState("instructions");
     
     const fetchDetails = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
@@ -17,18 +20,41 @@ function Recipe() {
         fetchDetails();
     }, [params.name]);
 
-  return (
-    <DetailWrapper>
-        <div>
-            <h2>{details.title}</h2>
-            <img src={details.image} alt="" />
-        </div>
-        <Info>
-            <Button>Instructions</Button>
-            <Button>Ingredients</Button>
-        </Info>
-    </DetailWrapper>
-  )
+    return (
+        <DetailWrapper>
+            <div>
+                <h2>{details.title}</h2>
+                <img src={details.image} alt="" />
+            </div>
+            <Info>
+                <Button 
+                    className={activeTab === "instructions" ? "active" : ""} 
+                    onClick={() => setActiveTab("instructions")}
+                >
+                    Instructions
+                </Button>
+                <Button 
+                    className={activeTab === "ingredients" ? "active" : ""} 
+                    onClick={() => setActiveTab("ingredients")}
+                >
+                    Ingredients
+                </Button>
+                {activeTab === "instructions" && (
+                    <div>
+                        <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
+                        <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
+                    </div>
+                )}
+                {activeTab === "ingredients" && (
+                    <ul>
+                        {details.extendedIngredients.map(ingredient => (
+                        <li key={ingredient.id}>{ingredient.original}</li>
+                    ))}
+                    </ul>
+                )}
+            </Info>
+        </DetailWrapper>
+    )
 }
 
 const DetailWrapper = styled.div`
