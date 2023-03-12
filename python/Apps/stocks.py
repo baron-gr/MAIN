93e8af400cb40af27d3fd34c764d9bde36edcb87
baron-gr <barongracias07@yahoo.co.uk>
2023-03-12 01:03:29 +0000

    12/03 updates

---
+++ python/Apps/stocks.py
@@ -0,0 +1,60 @@
+import streamlit as st
+from datetime import date
+import yfinance as yf
+from plotly import graph_objs as go
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+import numpy as np
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Price Prediction App")
+
+stocks = ("AAPL", "AMZN", "GOOG", "MSFT")
+selected_stock = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 1, 4)
+period = n_years * 365
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, start=START, end=TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Loading data...")
+data = load_data(selected_stock)
+data_load_state.text("Data loaded!")
+
+st.subheader("Raw data")
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig, use_container_width=True)
+
+plot_raw_data()
+
+# Forecasting
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast Data')
+st.write(forecast.tail())
+
+st.write('forecast data')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1, use_container_width=True)
+
+st.write('forecast components')
+fig2 = m.plot_components(forecast)
+st.write(fig2)
