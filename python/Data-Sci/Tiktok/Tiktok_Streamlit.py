cf56b32935bcbb76bd956b7eef0495cfc6b1f18c
baron-gr <barongracias07@yahoo.co.uk>
2024-02-13 22:14:15 +0000

    changes

--- python/Data-Sci/Tiktok/Tiktok_Streamlit.py
+++
@@ -1,8 +0,0 @@
-## Importing TikTok Python SDK and JSON to export data
-from TikTokApi import TikTokApi as tiktok
-import json
-
-## Cookie Data
-verifyFp = ""
-## Set up instance
-api = tiktok.get_instance(custom_verifyFp=verifyFp, use_test_endpoint=True)
