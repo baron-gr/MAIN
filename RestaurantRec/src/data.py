587af14e00dce65a87605d90be344a16a9663900
baron-gr <barongracias07@yahoo.co.uk>
2024-12-17 23:04:31 +0000

    cv

--- RestaurantRec/src/data.py
+++ RestaurantRec/src/data.py
@@ -2,7 +2,9 @@ import sqlite3
 import pandas as pd
 
 conn = sqlite3.connect('restaurants.db')
-df = pd.read_sql_query("SELECT count(*) FROM restaurants", conn)
+df = pd.read_sql_query("SELECT * FROM restaurants", conn)
 conn.close()
-print(df)
-# print(df[df['neighbourhood'] == 'Holborn'].sort_values(by=['rating', 'num_reviews'], ascending=False))
+
+print(df[df['neighbourhood'] == 'Soho'].sort_values(by=['rating', 'num_reviews'], ascending=False))
+
+# print(df[df['neighbourhood'] == 'Soho'].size)
